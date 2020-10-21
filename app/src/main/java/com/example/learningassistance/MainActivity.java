17fa4f18d0da8898b443836d7a93f8ffbefbac76
JinFengHua <2116161338@qq.com>
2020-10-21 21:08:30 +0800

    实现底部导航栏

--- app/src/main/java/com/example/learningassistance/MainActivity.java
+++ app/src/main/java/com/example/learningassistance/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.TextView;
 @SuppressLint("ResourceAsColor")
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private LinearLayout layout_homepage,layout_message,layout_dynamic,layout_my;
     private ImageView image_homepage,image_message,image_dynamic,image_my;
     private TextView text_homepage,text_message,text_dynamic,text_my;
 
@@ -27,14 +28,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         setContentView(R.layout.activity_main);
         fm = getSupportFragmentManager();
         initComponent();
-
     }
 
     private void initComponent() {
-        LinearLayout layout_homepage = findViewById(R.id.menu_homepage);
-        LinearLayout layout_dynamic = findViewById(R.id.menu_dynamic);
-        LinearLayout layout_message = findViewById(R.id.menu_message);
-        LinearLayout layout_my = findViewById(R.id.menu_my);
+        layout_homepage = findViewById(R.id.menu_homepage);
+        layout_dynamic = findViewById(R.id.menu_dynamic);
+        layout_message = findViewById(R.id.menu_message);
+        layout_my = findViewById(R.id.menu_my);
 
         image_homepage = findViewById(R.id.homepage_image);
         text_homepage = findViewById(R.id.homepage_text);
@@ -54,6 +54,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         layout_message.setOnClickListener(this);
         layout_dynamic.setOnClickListener(this);
         layout_homepage.setOnClickListener(this);
+        layout_homepage.performClick();
     }
 
     private void refreshView() {
@@ -85,33 +86,67 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         }
     }
 
-    public void clickHomepage(View view) {
-        refreshView();
-        image_homepage.setImageResource(R.drawable.icon_homepage_pressed);
-        text_homepage.setTextColor(R.color.selected);
-    }
-
-    public void clickMessage(View view) {
-        refreshView();
-        image_message.setImageResource(R.drawable.icon_message_pressed);
-        text_message.setTextColor(R.color.selected);
-    }
-
-    public void clickDynamic(View view) {
-        refreshView();
-        image_dynamic.setImageResource(R.drawable.icon_dynamic_pressed);
-        text_dynamic.setTextColor(R.color.selected);
-    }
-
-    public void clickMy(View view) {
-        refreshView();
-        image_my.setImageResource(R.drawable.icon_my_pressed);
-        text_my.setTextColor(R.color.selected);
+    public void setSelected(){
+        layout_homepage.setSelected(false);
+        layout_message.setSelected(false);
+        layout_dynamic.setSelected(false);
+        layout_my.setSelected(false);
     }
 
     @Override
     public void onClick(View v) {
         FragmentTransaction transaction = fm.beginTransaction();
         hideAllFragment(transaction);
+        refreshView();
+        setSelected();
+        switch (v.getId()){
+            case R.id.menu_homepage:
+                image_homepage.setImageResource(R.drawable.icon_homepage_pressed);
+                text_homepage.setTextColor(R.color.selected);
+                layout_homepage.setSelected(true);
+                if (f1 == null){
+                    f1 = new MyFragment("这是一个Fragment");
+                    transaction.add(R.id.fragment,f1);
+                } else {
+                    transaction.show(f1);
+                }
+                break;
+            case R.id.menu_message:
+                image_message.setImageResource(R.drawable.icon_message_pressed);
+                text_message.setTextColor(R.color.selected);
+                layout_message.setSelected(true);
+                if (f2 == null){
+                    f2 = new MyFragment("这是一个Fragment");
+                    transaction.add(R.id.fragment,f2);
+                } else {
+                    transaction.show(f2);
+                }
+                break;
+            case R.id.menu_dynamic:
+                image_dynamic.setImageResource(R.drawable.icon_dynamic_pressed);
+                text_dynamic.setTextColor(R.color.selected);
+                layout_dynamic.setSelected(true);
+                if (f3 == null){
+                    f3 = new MyFragment("这是一个Fragment");
+                    transaction.add(R.id.fragment,f3);
+                } else {
+                    transaction.show(f3);
+                }
+                break;
+            case R.id.menu_my:
+                image_my.setImageResource(R.drawable.icon_my_pressed);
+                text_my.setTextColor(R.color.selected);
+                layout_my.setSelected(true);
+                if (f4 == null){
+                    f4 = new MyFragment("这是一个Fragment");
+                    transaction.add(R.id.fragment,f4);
+                } else {
+                    transaction.show(f4);
+                }
+                break;
+            default:
+                break;
+        }
+        transaction.commit();
     }
 }
