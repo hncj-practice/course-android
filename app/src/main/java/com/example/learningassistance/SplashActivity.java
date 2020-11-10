cbc631db52d40028514fca7c103d5b7317c00fd6
JinFengHua <2116161338@qq.com>
2020-11-10 20:19:14 +0800

    增加欢迎页

---
+++ app/src/main/java/com/example/learningassistance/SplashActivity.java
@@ -0,0 +1,18 @@
+package com.example.learningassistance;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        startActivity(new Intent(this, Login.class));
+        finish();
+    }
+}
